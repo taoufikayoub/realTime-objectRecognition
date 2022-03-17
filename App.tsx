0607bb56fd65de2a30872bc07408894044c4585d
AyoubTaoufik <ayoub107p5@gmail.com>
2022-03-18 00:28:13 +0100

    first commit

--- App.tsx
+++ App.tsx
@@ -1,11 +1,96 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from "react";
+import { StyleSheet, Text, View, Platform } from "react-native";
+import { Camera } from "expo-camera";
+import * as tf from "@tensorflow/tfjs";
+import * as mobilenet from "@tensorflow-models/mobilenet";
+import { cameraWithTensors } from "@tensorflow/tfjs-react-native";
+
+const textureDims =
+  Platform.OS === "ios"
+    ? {
+        height: 1920,
+        width: 1080,
+      }
+    : {
+        height: 1200,
+        width: 1600,
+      };
+
+let frame = 0;
+const computeRecognitionEveryNFrames = 60;
+
+const TensorCamera = cameraWithTensors(Camera);
+
+const initialiseTensorflow = async () => {
+  await tf.ready();
+  tf.getBackend();
+};
 
 export default function App() {
+  const [hasPermission, setHasPermission] = useState<null | boolean>(null);
+  const [detections, setDetections] = useState<string[]>([]);
+  const [net, setNet] = useState<mobilenet.MobileNet>();
+
+  const handleCameraStream = (images: IterableIterator<tf.Tensor3D>) => {
+    const loop = async () => {
+      if (net) {
+        if (frame % computeRecognitionEveryNFrames === 0) {
+          const nextImageTensor = images.next().value;
+          if (nextImageTensor) {
+            const objects = await net.classify(nextImageTensor);
+            if (objects && objects.length > 0) {
+              setDetections(objects.map((object) => object.className));
+            }
+            tf.dispose([nextImageTensor]);
+          }
+        }
+        frame += 1;
+        frame = frame % computeRecognitionEveryNFrames;
+      }
+
+      requestAnimationFrame(loop);
+    };
+    loop();
+  };
+
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(status === "granted");
+      await initialiseTensorflow();
+      setNet(await mobilenet.load({ version: 1, alpha: 0.25 }));
+    })();
+  }, []);
+
+  if (hasPermission === null) {
+    return <View />;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+  if (!net) {
+    return <Text>Model not loaded</Text>;
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TensorCamera
+        style={styles.camera}
+        onReady={handleCameraStream}
+        type={Camera.Constants.Type.back}
+        cameraTextureHeight={textureDims.height}
+        cameraTextureWidth={textureDims.width}
+        resizeHeight={200}
+        resizeWidth={152}
+        resizeDepth={3}
+        autorender={true}
+        useCustomShadersToResize={false}
+      />
+      <View style={styles.text}>
+        {detections.map((detection, index) => (
+          <Text key={index}>{detection}</Text>
+        ))}
+      </View>
     </View>
   );
 }
@@ -13,8 +98,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    flex: 1,
+  },
+  camera: {
+    flex: 10,
+    width: "100%",
   },
 });
